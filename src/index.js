9fff0e262ff9332c0dc00c1c27c987a508602849
MrUser <user1@localhost.localdomain>
2018-01-28 21:25:40 -0600

    :octocat: pullBranch

--- src/index.js
+++ src/index.js
@@ -4,11 +4,7 @@ import App from './App';
 import './index.css';
 import state from './state';
 
-//
-// function increaseCart(){
-//   console.log(state.numberOfItemsInCart +=1);
-//   reactDom.render(<App state={state} />, document.getElementById("root"));
-// }
+
 
 ReactDOM.render(<App products={state.products} />, document.getElementById('root'));
 //this variabel "products" is equal to {state.products} and when using variable name "props"will preceed. i.e. props.product.
